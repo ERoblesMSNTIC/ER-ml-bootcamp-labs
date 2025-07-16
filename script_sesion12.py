60e48c1a6fa1d7711fcf5651b038b505ecb3382e
ERoblesMSNTIC <115627512+ERoblesMSNTIC@users.noreply.github.com>
2025-07-16 23:00:04 +0000

    Cambios 2 Sesion 12

--- script_sesion12.py
+++ script_sesion12.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
@@ -21,6 +23,7 @@ from sklearn.metrics import accuracy_score, recall_score, f1_score
 df = pd.read_csv('data/raw/Operational_events.csv')
 
 # 2. EDA de datos
+# Para este archivo no hay EDA
 
 # 3. Transformaciones
 # 3.1 Reducción del dataframe
@@ -77,6 +80,69 @@ xgb_model.fit(X_train, y_train)
 xgb_y_pred = xgb_model.predict(X_test)
 
 print("Model XGB Done!")
+
+# Fine-tuning de los modelos
+# Implementación de GridSearchCV en ambos modelos
+
+# RF fine tuning
+
+rf_base = RandomForestClassifier(
+    random_state=23
+)
+
+rf_param_grid = {
+    'n_estimators': [500, 1000, 2500],
+    'max_depth': [None, 10],
+    'min_samples_split': [2,5] ,
+}
+
+rf_grid = GridSearchCV(
+    rf_base,
+    param_grid=rf_param_grid,
+    cv= 3, # Cross validation Número de grupos en los que se subdivide el dataset
+    scoring= "f1_weighted", # Función objetivo a minimizar - F1 es para balanceo de clases
+    n_jobs= 1, # Número de nucleos
+    verbose=2 #
+)
+
+
+rf_grid.fit(X_train,y_train)
+
+best_rf_grid = rf_grid.best_estimator_ # Esto ya es un modelo para predecir
+
+print("Mejores valores para RF:")
+print(best_rf_grid)
+
+rf_grid_y_pred = best_rf_grid.predict(X_test)
+
+
+# XGBoost fine tuning
+
+xgb_base = XGBClassifier(eval_metric="mlogloss", random_state=23)
+
+xgb_param_grid = {
+    'n_estimators': [500, 1000, 2500],
+    'max_depth': [None, 10, 20],
+    'learning_rate': [0.01, 0.1, 0.25] ,
+}
+
+xgb_grid = GridSearchCV(
+    xgb_base,
+    param_grid=rf_param_grid,
+    cv= 3, # Cross validation Número de grupos en los que se subdivide el dataset
+    scoring= "f1_weighted", # Función objetivo a minimizar - F1 es para balanceo de clases
+    n_jobs= 1, # Número de nucleos
+    verbose=2 #
+)
+
+xgb_grid.fit(X_train, y_train)
+best_xgb_grid = xgb_grid.best_estimator_
+
+xgb_grid_y_pred = best_xgb_grid.predict(X_test)
+
+print(best_xgb_grid)
+
+
 # 5. Evaluación
 
 def model_evaluation(nombre,y_true,y_pred):
@@ -91,5 +157,6 @@ def model_evaluation(nombre,y_true,y_pred):
 
 model_evaluation("Random Forest",y_test,rf_y_pred)
 model_evaluation("XGBoost",y_test,xgb_y_pred)
-
+model_evaluation("Random Forest con GridSearch",y_test,rf_grid_y_pred)
+model_evaluation("XGBoost con GridSearch",y_test,xgb_grid_y_pred)
 print('OK')
